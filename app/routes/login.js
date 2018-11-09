922a7414370918f29402b8dfef5fb1d165fb6e75
firstein <alienor.brabant@gmail.com>
2018-11-09 12:59:11 +0100

    modif login.ejs

--- app/routes/login.js
+++ app/routes/login.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Login Page' });
+  res.render('authentification/login', { title: 'Login Page' });
+});
+
+/* GET login page. */
+router.get('/create', function(req, res, next) {
+  res.render('authentification/createLogin', { title: 'Create Account' });
 });
 
 module.exports = router;
