1d11cf38bfbada391e726b47d6409ee91194b708
firstein <alienor.brabant@gmail.com>
2018-11-09 08:54:56 +0100

    delete users create by express-generator

--- app/app.js
+++ app/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 //Liste des routes de l'app
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var projetRouter = require('./routes/projet');
 var loginRoutes = require('./routes/login')
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Binding des routes de l'app
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/projet', projetRouter);
 app.use('/login', loginRoutes);
 
