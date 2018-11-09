22a5f5ed8d0b680e883e60b9a3485beb1b992d37
firstein <alienor.brabant@gmail.com>
2018-11-09 16:26:12 +0100

    login fonctionne mais on ne peut pas créer de compte

--- app/routes/login.js
+++ app/routes/login.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
-  res.render('authentification/login', { title: 'Login Page' });
+  res.render('authentication/login', { title: 'Login Page' });
 });
 
 /* GET login page. */
 router.get('/create', function(req, res, next) {
-  res.render('authentification/createLogin', { title: 'Create Account' });
+  res.render('authentication/createAccount', { title: 'Create Account' });
 });
 
 module.exports = router;
