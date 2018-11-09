1a4733ac1d65d70fe2c075d563cc7c244e75ed1a
Carlos Villavicencio <cvillavicenc@u-bordeaux.fr>
2018-11-09 03:31:09 +0100

    remove firebase connection

--- app/routes/projet.js
+++ app/routes/projet.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const firebase = require('firebase');
-const config = {
-  apiKey: "AIzaSyC-0azPBpoSpT1AfPI85FTDT-vb0Q3CDlY",
-  authDomain: "conduiteprojet2018-ubx.firebaseapp.com",
-  databaseURL: "https://conduiteprojet2018-ubx.firebaseio.com",
-  projectId: "conduiteprojet2018-ubx",
-  storageBucket: "conduiteprojet2018-ubx.appspot.com",
-  messagingSenderId: "597745430634"
-};
-//let db = firebase.initializeApp(config).database(); //sans doute faux
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('projet', { title: 'Project List' });
